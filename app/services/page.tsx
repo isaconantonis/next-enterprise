6cba26b3b9bf269f85e002c7e4f3879436cba352
Sentientsoap <n.eliaerts@gmail.com>
2024-06-19 16:17:53 +0200

    styling/general-changes-2

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,30 +1,6 @@
 'use client'
-import { BuildingStorefrontIcon, CloudArrowUpIcon, LightBulbIcon, PaintBrushIcon } from "@heroicons/react/24/solid"
 import PricingPlans from "../../components/PricingPlans/PricingPlans"
-const features = [
-  {
-    name: "Web & E-commerce",
-    description:
-      "Elevating businesses online with tailored solutions ranging from basic websites to fully customized E-commerce stores and expert Shopify development.",
-    icon: BuildingStorefrontIcon,
-  },
-  {
-    name: "Branding & design",
-    description: "With a focus on thoughtful design, our services bring vitality to our clients' brands. We specialize in creating distinctive identities that leave a lasting impression, reflecting the essence of each story.",
-    icon: PaintBrushIcon,
-  },
-  {
-    name: "Cloud based setup",
-    description: "Empower your software with our cloud-based services, bringing it to new heights within the boundless expanse of the cloud. We seamlessly integrate cutting-edge technologies, ensuring scalability, security, and unrivaled performance for your digital solutions.",
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: "Your vision quantified",
-    description:
-      "Experience a warm welcome at our office, savor a cup of coffee, and share your views on your online presence. Together, we'll uncover the perfect solution for you or your enterprise",
-    icon: LightBulbIcon,
-  },
-]
+import services from '../../data/servicesData'
 
 export default async function Projects() {
   return (
@@ -41,7 +17,7 @@ export default async function Projects() {
               <PricingPlans/>
               <div className="mx-auto mt-8 max-w-2xl sm:mt-20 lg:mt-14 lg:max-w-5xl">
                 <dl className="mx-auto grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none md:grid-cols-2 lg:gap-y-16">
-                  {features.map((feature) => (
+                  {services.map((feature) => (
                     <div key={feature.name} className="relative flex flex-col">
                       <div className="mx-auto mb-4 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-800">
                         <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />

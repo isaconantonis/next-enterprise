3e80d81939a06c28f2dd8d1782bcf6c35891d155
Sentientsoap <n.eliaerts@gmail.com>
2024-07-03 16:55:48 +0200

    milestone/before-officeLightbox-breakage

--- components/officeLightbox/OfficeLightbox.tsx
+++ components/officeLightbox/OfficeLightbox.tsx
@@ -5,15 +5,16 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 
 const OfficeLightbox: React.FC = () => {
+  
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const images = [
+  const media = [
     '/office1.jpeg',
     '/office5.jpeg',
     '/office3.jpeg',
     '/office2.jpeg',
     '/office6.jpeg',
-    '/office8.jpeg'
+    '/office8.jpeg',
   ];
   const openLightbox = (image: string, index: number) => {
     setSelectedImage(image);
@@ -24,18 +25,18 @@ const OfficeLightbox: React.FC = () => {
   };
   const handlePrev = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
-    setCurrentIndex(prevIndex => (prevIndex === 0 ? images.length -1 : prevIndex -1));
-    setSelectedImage(images[currentIndex -1 < 0 ? images.length -1 : currentIndex -1]);
+    setCurrentIndex(prevIndex => (prevIndex === 0 ? media.length -1 : prevIndex -1));
+    setSelectedImage(media[currentIndex -1 < 0 ? media.length -1 : currentIndex -1]);
   };
   const handleNext = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation(); 
-    setCurrentIndex(prevIndex => (prevIndex === images.length -1 ? 0 : prevIndex +1));
-    setSelectedImage(images[currentIndex +1 >= images.length ? 0 : currentIndex +1]); 
+    setCurrentIndex(prevIndex => (prevIndex === media.length -1 ? 0 : prevIndex +1));
+    setSelectedImage(media[currentIndex +1 >= media.length ? 0 : currentIndex +1]); 
   };
 
   return (
   <div className='md:px-60 lg:px-72 xl:px-96 pt-10'>
-    <div className='text-center'>
+    <div className='text-center px-2 sm:px-0'>
       <h2 className='text-3xl font-bold tracking-tight text-primary-800 sm:text-4xl'>Looking for office space?<br/> we have you covered!</h2>
       <p className='mt-4 text-lg leading-8 text-primary-800'>Explore a practical coworking space in the heart of Antwerp's Kammenstraat. Our cozy office setup is designed for productivity, offering a limited number of spots. Secure your place in the city center and be part of a workspace that balances comfort and inspiration.</p>
       <div className='mt-10 flex'>
@@ -44,7 +45,7 @@ const OfficeLightbox: React.FC = () => {
     </div>
     <div className='my-4 p-4 relative sm:p-4 md:p-0 lg:p-0 xl:p-0 2xl:p-0 h-auto'>
       <div className="grid grid-cols-2 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 gap-1">
-        {images.map((image, index) => (
+        {media.map((image, index) => (
           <Image
             key={index}
             src={image}
